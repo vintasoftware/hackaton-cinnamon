42cc2581a8ab0af06f148eff398a92b4aa431abe
Flávio Juvenal <flaviojuvenal@gmail.com>
2016-02-20 17:20:35 -0300

    Alchemy complete example

--- github_alchemy.py
+++ github_alchemy.py
@@ -4,15 +4,39 @@ from bs4 import BeautifulSoup
 import re
 
 issues_url = 'https://api.github.com/repos/kennethreitz/requests/issues'
+apikey = ''
 
 response = requests.get(issues_url)
-response_body = response.json()
-# assert '```' in response_body
-# print('before\n', response_body)
+issue_list = response.json()[:5]
+issue_tag_dict = {}
+for issue in issue_list:
+    # remove code blocks
+    issue_body = re.sub(r'```.+```', '', issue['body'], flags=re.DOTALL)
 
-# print('\n'*10)
-# print('\n\n\nafter\n', re.sub(r'```.+```', '', response_body, flags=re.DOTALL))
-re.sub(r'```.+```', '', response_body, flags=re.DOTALL)
+    response = requests.get(
+        'http://gateway-a.watsonplatform.net/calls/text/TextGetRankedKeywords',
+        params={
+            'apikey': apikey,
+            'text': issue_body,
+            'maxRetrieve': 10,
+            'keywordExtractMode': 'strict',
+            'sentiment': 0,
+            'outputMode': 'json'
+        })
+    keywords = response.json()['keywords']
+    tags = sorted(
+        [(x['text'], x['relevance']) for x in keywords],
+        key=lambda t: t[1],
+        reverse=True)
+    issue_tag_dict[issue['number']] = {
+        'issue': issue['title'],
+        'text': issue['body'],
+        'tags': tags
+    }
+
+import pprint
+pprint.pprint(issue_tag_dict)
+
 
 # print('with code\n', gfm.markdown(response_body))
 # print('\n' * 10)
@@ -21,7 +45,7 @@ re.sub(r'```.+```', '', response_body, flags=re.DOTALL)
 # print('without code\n', )
 
 
-# apikey = ''
+# apikey = '7e01429feb1c1a6fd78aee6e1246bc25ff9dc5a5'
 # text = '''
 # Unicode QUERY_PARAM not parsed at all
 
