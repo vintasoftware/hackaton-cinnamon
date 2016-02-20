78d1f96e72a1e1af4bd4d7ef7de7602be2b975e5
Flávio Juvenal <flaviojuvenal@gmail.com>
2016-02-20 20:33:13 -0300

    models

--- issues/models.py
+++ issues/models.py
@@ -1,28 +1,45 @@
-# from __future__ import unicode_literals
-#
-# from django.db import models
-#
-#
-# class BikewayCategory(models.Model):
-#     name = models.CharField(max_length=255)
-#     is_separated = models.BooleanField()
-#
-#     def __unicode__(self):
-#         return self.name
-#
-#
-# class Bikeway(models.Model):
-#     CONDITION_CHOICES = [
-#         ('bad', "Bad"),
-#         ('regular', "Regular"),
-#         ('good', "Good")]
-#
-#     name = models.CharField(max_length=255)
-#     location = models.CharField(max_length=255)
-#     condition = models.CharField(max_length=255,
-#                                  choices=CONDITION_CHOICES)
-#     category = models.ForeignKey(BikewayCategory, related_name='bikeways')
-#     length = models.PositiveIntegerField()
-#
-#     def __unicode__(self):
-#         return "{} - {}".format(self.name, self.location)
+from django.db import models
+
+from core.models import IndexedTimeStampedModel
+
+
+class Issue(IndexedTimeStampedModel):
+    number = models.PositiveIntegerField()
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    repo_owner = models.CharField(max_length=255)
+    repo = models.CharField(max_length=255)
+    raw = models.TextField()
+
+    def __unicode__(self):
+        return "#{} - {}".format(self.number, self.title)
+
+
+class Tag(IndexedTimeStampedModel):
+    issue = models.ForeignKey(Issue, related_name='tags')
+    name = models.CharField(max_length=255)
+    relevance = models.FloatField()
+
+    def __unicode__(self):
+        return "{} ({})".format(self.name, self.relevance)
+
+
+class File(IndexedTimeStampedModel):
+    filename = models.CharField(max_length=4096)
+
+    def __unicode__(self):
+        return self.filename
+
+
+class PullRequest(IndexedTimeStampedModel):
+    number = models.PositiveIntegerField()
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    repo_owner = models.CharField(max_length=255)
+    repo = models.CharField(max_length=255)
+    issues = models.ManyToManyField(Issue, related_name='prs')
+    files = models.ManyToManyField(File, related_name='prs')
+    raw = models.TextField()
+
+    def __unicode__(self):
+        return "PR #{} - {}".format(self.number, self.title)
