0eef402b6551b6a8daf7a5645f05d1e4cd6ac1e2
Victor Carrico <victorcarrico@gmail.com>
2016-02-21 18:18:34 -0300

    Listing repos

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,10 @@
 
 from django.conf.urls import patterns, url
 
-from accounts.views import RepoListView
+from accounts.views import RepoListView, RepoCreateView, RepoDeleteView
 
 urlpatterns = patterns('',
     url(r'^list/$', RepoListView.as_view(), name='list'),
+    url(r'^create/$', RepoCreateView.as_view(), name='create'),
+    url(r'^delete/$', RepoDeleteView.as_view(), name='delete'),
 )
