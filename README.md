a8489b82a2c04e504c253baf94f70dc9dc792be4
Victor Carrico <victorcarrico@gmail.com>
2016-02-22 01:21:48 -0300

    Changing github message

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 - `heroku git:remote -a app-name-here`
 - `heroku config:set DJANGO_SETTINGS_MODULE=core.settings.production`
 - `heroku config:set 'SECRET_KEY=secret-key-here'`
+- `heroku config:set 'CINNABOT_TOKEN=cinnabot-github-token'`
 - `heroku addons:create heroku-postgresql:hobby-dev`
 - `git push heroku master`
 - `heroku run python manage.py migrate`
--- accounts/allauth.py
+++ accounts/allauth.py
@@ -7,27 +7,23 @@ from django.template import Context, Template
 from allauth.account.adapter import DefaultAccountAdapter
 
 
-MESSAGE_1 = """
+MESSAGE = """
 Here is our recommendation for the assignment ordinated by performance and availability:
 {% for user in users %}
 {{ forloop.counter }}. @{{ user }}
 {% endfor %}
 Assign the issue to someone. After this step we'll send more informations to help the coder.
-Thanks,
 
-Cinnabot"""
-
-MESSAGE_2 = """
-Hi @{{ developer }},
-we've a few suggestions to helps you to solve this issue!
+We've a few suggestions to helps you to solve this issue!
 Here are a list with similar issues:
 {% for issue in issues %}
-{{ forloop.counter }}. #{{ issue }}
+{{ forloop.counter }}. [{{ issue.name }}]({{ issue.link }})
 {% endfor %}
 Possible files where you'll need to work:
 {% for file in files %}
-{{ forloop.counter }}. {{ file }}
+{{ forloop.counter }}. [{{ file.name }}]({{ file.link }})
 {% endfor %}
+
 We hope have helped!
 Thanks,
 
@@ -40,18 +36,12 @@ class AccountAdapter(DefaultAccountAdapter):
         return '/repo/list/'
 
 
-def comment_on_issue(user, repo, issue, msg_num, users=[], developer=None, issues=[], files=[]):
+def comment_on_issue(user, repo, issue, users, issues, files):
     url = 'https://api.github.com/repos/{}/{}/issues/{}/comments'.format(user, repo, issue)
     headers = {'Authorization': 'token {}'.format(settings.CINNABOT_TOKEN)}
 
-    if msg_num == 1:
-        template = Template(MESSAGE_1)
-        c = Context({'users': users})
-    elif msg_num == 2:
-        template = Template(MESSAGE_2)
-        c = Context({'developer': developer, 'issues': issues, 'files': files})
-    else:
-        return 'message num > 2'
+    template = Template(MESSAGE)
+    c = Context({'users': users, 'issues': issues, 'files': files})
     message = template.render(c)
 
     data = json.dumps({'body': message})
